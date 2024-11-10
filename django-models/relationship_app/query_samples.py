a55ce68db4113002aecab5cfdd0b83a26ddfcf4b
Immanuel Njogu <njoguimmanuel8@gmail.com>
2024-11-11 00:12:17 +0300

    changes

--- django-models/relationship_app/query_samples.py
+++ django-models/relationship_app/query_samples.py
@@ -24,3 +24,13 @@ books_by_author = Book.objects.filter(author=author)  # Correct filter usage
 # Print out the books by this author
 for book in books_by_author:
     print(f"Book Title: {book.title}, Author: {book.author.name}")
+
+    # Example: Query to find the librarian for a specific library
+library_name = "Central Library"  # Change this to the name of the library you want
+library = Library.objects.get(name=library_name)
+
+# Retrieve the librarian for this library
+librarian = Librarian.objects.get(library=library)
+
+# Print out the librarian's details
+print(f"Librarian for {library.name}: {librarian.name}")
