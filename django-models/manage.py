e97abb1fba842d950bb243e395ba4d0de0a6e715
Immanuel Njogu <njoguimmanuel8@gmail.com>
2024-11-12 07:01:49 +0300

    task 1

--- django-models/manage.py
+++ django-models/manage.py
@@ -7,6 +7,8 @@ import sys
 def main():
     """Run administrative tasks."""
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'LibraryProject.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django-models.settings')
+
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
