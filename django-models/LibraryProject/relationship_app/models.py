c67f01c79622ac18286d425b866068e1223fe5ae
Immanuel Njogu <njoguimmanuel8@gmail.com>
2024-11-14 23:00:29 +0300

    task 0

--- django-models/LibraryProject/relationship_app/models.py
+++ django-models/LibraryProject/relationship_app/models.py
@@ -5,12 +5,18 @@ from django.db import models
 # name: CharField.
 class Author(models.Model):
     name = models.CharField(max_length = 100)
+
+    def __str__(self):
+        return self.name
 # Book Model:
 # title: CharField.
 # author: ForeignKey to Author.
 class Book(models.Model):
     title = models.CharField(max_length = 100)
     author = models.ForeignKey(Author, on_delete = models.CASCADE)
+    def __str__(self):
+        return self.title
+    
 
 
 # Library Model:
@@ -19,10 +25,16 @@ class Book(models.Model):
 class Library(models.Model):
     name = models.CharField(max_length = 100)
     book = models.ManyToManyField(Book)
+
+    def __str__(self):
+        return self.name
 # Librarian Model:
 # name: CharField.
 # library: OneToOneField to Library
 class Librarian(models.Model):
     name = models.CharField(max_length = 100)
     library = models.OneToOneField(Library, on_delete = models.CASCADE)
+
+    def __str__(self):
+        return self.name
 
