27be9afb32c092a2830619da4dba0474e0fee5fd
Immanuel Njogu <njoguimmanuel8@gmail.com>
2024-11-16 20:41:28 +0300

    changes tasks 3

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -5,7 +5,7 @@ from .models import Book
 from django.contrib.auth.forms import UserCreationForm
 from django.urls import reverse_lazy
 from django.views.generic import CreateView
-#from django.contrib.auth import login
+from django.contrib.auth import login
 from .models import Library
 
 
