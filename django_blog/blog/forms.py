a2fd53a5fce267823627c476b65b56715322160f
Immanuel N <immanuelnjogu@students.tukenya.ac.ke>
2024-12-07 22:55:55 +0300

    task 2. Creating Blog Post Management Features

--- django_blog/blog/forms.py
+++ django_blog/blog/forms.py
@@ -1,6 +1,9 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User 
+from .models import Comment,Post
+from taggit.forms import TagWidget 
+
 
 #custom registration
 class CustomUser(UserCreationForm):
@@ -9,4 +12,22 @@ class CustomUser(UserCreationForm):
     class Meta:
         model = User
         fields = ['username','email','password1','password2']
-        
+
+# user update form to update username and email fields
+class UpdateForm(forms.ModelForm):
+        class Meta:
+            model = User
+            fields = ["username", "email", 'password' ]
+
+class PostForm(forms.ModelForm):
+     class Meta:
+          model = Post
+          fields = ['title', 'content', 'tags']
+          widgets = {
+            'tags': TagWidget(),
+          }
+
+class CommentForm(forms.ModelForm):
+     class Meta:
+          model = Comment
+          fields = ['content']
--- django_blog/blog/models.py
+++ django_blog/blog/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from taggit.managers import TaggableManager
 # Create your models here.
 
 
@@ -8,6 +9,8 @@ class Post(models.Model):
     content = models.TextField()
     published_date = models.DateTimeField(auto_now_add=True)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    tags = TaggableManager()
 
     def __str__(self):
         return self.title
@@ -19,3 +22,17 @@ class Profile(models.Model):
 
     def __str__(self):
         return f'{self.user.username} Profile'
+    
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='post_comments')
+    author =models.ForeignKey(User, on_delete=models.CASCADE, related_name='author_comments')
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now=True)
+    updated_at = models.DateTimeField(auto_now_add=True)
+
+class Tag(models.Model):
+    name = models.ManyToManyField(Post, verbose_name='list of post_tags')
+
+    def __str__(self):
+        return self.name
