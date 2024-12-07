92d74776f787d38533ee8b17cc74ee5c922d8d5d
Immanuel N <immanuelnjogu@students.tukenya.ac.ke>
2024-12-07 22:58:50 +0300

    task 2. Creating Blog Post Management Features

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -9,4 +9,23 @@ urlpatterns = [
     path('login/', views.login_view, name='login'),
     path('logout/', views.logout_view, name='logout'),
     path('profile/', ProfileView.as_view(), name='profile'),
+
+     # path urls for CRUD actions on posts
+    path('post/new/', views.PostCreateView.as_view(), name='post-create'),
+    path('post/list-view/', views.PostListView.as_view(), name='post-list'),
+    path('post/<int:pk>/detail/', views.PostDetailView.as_view(), name='post-detail'),
+    path('post/<int:pk>/update/', views.PostUpdateView.as_view(), name='post-update'),
+    path('post/<int:pk>/delete/', views.PostDeleteView.as_view(), name='post-delete'),
+
+    # path urls for CRUD actions on comments
+    path('post/<int:pk>/comments/new/', views.CommentCreateView.as_view(), name='comment-create'),
+    path('post/comment-list-view/', views.CommentListView.as_view(), name='comment-list'),
+    # path('post/<int:pk>/comment-detail/', views.CommentDetailView.as_view(), name='post-comment-detail'),
+    path('comment/<int:pk>/update/', views.CommentUpdateView.as_view(), name='post-comment-update'),
+    path('comment/<int:pk>/delete/', views.CommentDeleteView.as_view(), name='post-comment-delete'),
+
+    # path urls to filter posts by tags
+    path('tag/<str:tag_name>/', views.post_by_tag, name='post-by-tag'),
+    path('tags/<slug:tag_slug>/', views.PostByTagListView.as_view(), name='posts-by-tag'),
+
 ]
