024fc7e575301b14697f111cdbfa632b602b24b2
Immanuel N <immanuelnjogu@students.tukenya.ac.ke>
2025-01-03 08:26:02 +0300

    changes

--- social_media_api/social_media_api/settings.py
+++ social_media_api/social_media_api/settings.py
@@ -86,10 +86,15 @@ DEBUG = False
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'socialmedia',
+        'USER': 'root',
+        'PASSWORD': 'Immamanu1234!',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
+
 
 
 # Password validation
